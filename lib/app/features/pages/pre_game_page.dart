bd75152f3d1d8f44194f4795a54e25260f70356d
Bogusz Brzóska <bogusz.brzoska@gmail.com>
2024-01-03 00:51:31 +0100

    implement parts of ui

--- lib/app/features/pages/pre_game_page.dart
+++ lib/app/features/pages/pre_game_page.dart
@@ -16,78 +16,70 @@ class PreGamePage extends StatelessWidget {
         ),
         backgroundColor: Color.fromARGB(255, 27, 58, 93),
       ),
-      body: Column(
-        children: [
-          Center(
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height / 3,
-              width: MediaQuery.of(context).size.width / 2,
+      body: Center(
+        child: Column(
+          children: [
+            Text("Choose category:",
+                style: Theme.of(context).textTheme.displayMedium),
+            Text("Choose difficulty:",
+                style: Theme.of(context).textTheme.displayMedium),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.75,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(100),
+                  border: Border.all(width: 4, color: Colors.black)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Easy", style: Theme.of(context).textTheme.displaySmall),
+                  Image(
+                    image: AssetImage("images/leave.png"),
+                    width: MediaQuery.of(context).size.width / 8,
+                  )
+                ],
+              ),
             ),
-          ),
-          Column(
-            children: [
-              Text("Choose difficulty:",
-                  style: Theme.of(context).textTheme.displayMedium),
-              Container(
-                width: MediaQuery.of(context).size.width / 1.75,
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(100),
-                    border: Border.all(width: 4, color: Colors.black)),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Easy",
-                        style: Theme.of(context).textTheme.displaySmall),
-                    Image(
-                      image: AssetImage("images/leave.png"),
-                      width: MediaQuery.of(context).size.width / 8,
-                    )
-                  ],
-                ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.75,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.circular(100),
+                  border: Border.all(width: 4, color: Colors.black)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Medium",
+                      style: Theme.of(context).textTheme.displaySmall),
+                  Image(
+                    image: AssetImage("images/bitten_leaf_scaled.png"),
+                    width: MediaQuery.of(context).size.width / 8,
+                  )
+                ],
               ),
-              Container(
-                width: MediaQuery.of(context).size.width / 1.75,
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                    color: Colors.yellow,
-                    borderRadius: BorderRadius.circular(100),
-                    border: Border.all(width: 4, color: Colors.black)),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Medium",
-                        style: Theme.of(context).textTheme.displaySmall),
-                    Image(
-                      image: AssetImage("images/bitten_leaf_scaled.png"),
-                      width: MediaQuery.of(context).size.width / 8,
-                    )
-                  ],
-                ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.75,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(100),
+                  border: Border.all(width: 4, color: Colors.black)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Hard", style: Theme.of(context).textTheme.displaySmall),
+                  Image(
+                    image: AssetImage("images/fire_leaf_scale.png"),
+                    width: MediaQuery.of(context).size.width / 8,
+                  )
+                ],
               ),
-              Container(
-                width: MediaQuery.of(context).size.width / 1.75,
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(100),
-                    border: Border.all(width: 4, color: Colors.black)),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Hard",
-                        style: Theme.of(context).textTheme.displaySmall),
-                    Image(
-                      image: AssetImage("images/fire_leaf_scale.png"),
-                      width: MediaQuery.of(context).size.width / 8,
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
