a23891cdfe9d601539148e5690c05d3febd284e6
Bogusz Brzóska <bogusz.brzoska@gmail.com>
2024-01-08 22:41:52 +0100

    Add reacting to the correct answer

--- lib/features/pages/question_page/question_page.dart
+++ lib/features/pages/question_page/question_page.dart
@@ -1,4 +1,5 @@
 import 'package:brain_check/app/injection_container.dart';
+import 'package:brain_check/domain/models/question_model.dart';
 import 'package:brain_check/features/pages/question_page/cubit/question_page_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -50,7 +51,10 @@ class QuestionPage extends StatelessWidget {
                       Wrap(
                         children: [
                           for (final answer in state.anserws) ...[
-                            AnswerWidget(answer: answer)
+                            AnswerWidget(
+                              answer: answer,
+                              questionModel: questionModel,
+                            )
                           ],
                         ],
                       ),
@@ -59,7 +63,7 @@ class QuestionPage extends StatelessWidget {
                           showDialog<void>(
                               context: context,
                               builder: (context) => AlertDialog(
-                                    content: Text("Correct Answer"),
+                                    content: Text(questionModel.correctAnswer),
                                   ));
                         },
                         child: Container(
@@ -97,14 +101,11 @@ class QuestionPage extends StatelessWidget {
 }
 
 class AnswerWidget extends StatefulWidget {
-  const AnswerWidget({
-    super.key,
-    required this.answer,
-
-  });
+  const AnswerWidget(
+      {super.key, required this.answer, required this.questionModel});
 
   final String answer;
- 
+  final QuestionModel questionModel;
 
   @override
   State<AnswerWidget> createState() => _AnswerWidgetState();
@@ -116,39 +117,68 @@ class _AnswerWidgetState extends State<AnswerWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Material(
-        shape: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-        clipBehavior: Clip.hardEdge,
-        child: InkWell(
-          onTap: () {
-            setState(() {
-              if (isChoosed == true) {
-                isChoosed = false;
-                
-              } else {
-                isChoosed = true;
-              }
-            });
-            print(widget.answer);
-          },
-          child: Container(
-              alignment: Alignment.center,
-              width: MediaQuery.of(context).size.width * 0.4,
-              height: MediaQuery.of(context).size.height * 0.15,
-              decoration: BoxDecoration(
-                border: Border.all(width: 2, color: Colors.black),
-                borderRadius: BorderRadius.circular(10),
-                color: isChoosed
-                    ? Colors.orange
-                    : Color.fromARGB(255, 56, 146, 249),
-              ),
-              child: Text(HtmlUnescape().convert(widget.answer),
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.bodyLarge)),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Material(
+            shape: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+            clipBehavior: Clip.hardEdge,
+            child: InkWell(
+              onTap: () async {
+                setState(() {
+                  if (isChoosed == true) {
+                    isChoosed = false;
+                  } else {
+                    isChoosed = true;
+                    asnwerName = widget.answer;
+                  }
+                });
+                print(widget.answer);
+                if (asnwerName == widget.questionModel.correctAnswer) {
+                  showDialog<void>(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                            content: Text("CORRECT!"),
+                          ));
+                } else {
+                  showDialog<void>(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                            content: Text(
+                                "WRONG!! Correct answer is: ${widget.questionModel.correctAnswer}"),
+                            actions: [
+                              IconButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  icon: Icon(Icons.arrow_back))
+                            ],
+                          ));
+                }
+              },
+              child: Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  height: MediaQuery.of(context).size.height * 0.15,
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: Colors.black),
+                      borderRadius: BorderRadius.circular(10),
+                      color: isChoosed &&
+                              asnwerName == widget.questionModel.correctAnswer
+                          ? Colors.green
+                          : isChoosed &&
+                                  asnwerName !=
+                                      widget.questionModel.correctAnswer
+                              ? Colors.red
+                              : Color.fromARGB(255, 56, 146, 249)),
+                  child: Text(HtmlUnescape().convert(widget.answer),
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.bodyLarge)),
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
