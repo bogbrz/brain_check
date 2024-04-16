7fadbb98657ea186ad41166684e91585aeb4103c
Bogusz Brzóska <bogusz.brzoska@gmail.com>
2024-04-16 20:10:39 +0200

    finish updating ui

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -5,6 +5,7 @@ import 'package:brain_check/features/pages/root_page/root_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
